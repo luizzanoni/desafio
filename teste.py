883ce20419e5ef9bcaa97bf5f528fc4cb7f64605
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-08-30 17:47:54 -0300

    introdução dos valores e prints em tela

--- teste.py
+++ teste.py
@@ -1,5 +1,17 @@
-print("15 exames coletados, e 7 exames prontos!")
-exames_prontos = input("Deseja encaminhar mensagem para os clientes (S/N)?")
+print("Total de 15 exames coletados, e 7 exames prontos!")
+opcao = input('Deseja enviar SMS para os clientes? 1 - Sim 2 - Não: ')
+if opcao == 1:
+    print("Encaminhando SMS")
+else:
+    print('Até mais!')
 
-examePronto = ['Luiz', '28 anos', 'Exames gerais de laboratório', '49999999999']
-print (examePronto)
+
+
+
+
+
+
+
+
+# examePronto = ['Luiz', '28 anos', 'Exames gerais de laboratório', '49999999999']
+# print (examePronto)
